dc0b3e23d49370cffc75527753279334667d1f20
Arjun <71964107+arjun-sasidharan@users.noreply.github.com>
2023-10-22 21:04:46 +0530

    Presentation Layer: Screen UI Added

---
+++ app/src/main/java/com/example/tmdbclient/presentation/tv/TvShowActivity.kt
@@ -0,0 +1,15 @@
+package com.example.tmdbclient.presentation.tv
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.tmdbclient.R
+import com.example.tmdbclient.databinding.ActivityTvShowBinding
+
+class TvShowActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTvShowBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_tv_show)
+    }
+}
